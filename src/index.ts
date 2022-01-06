14a803fc1fd776450deba12ff9d5a1a5d728cf16
NadiaElso <92063948+NadiaElso@users.noreply.github.com>
2022-01-06 19:48:29 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+console.log("Supermercado");
+console.log("productos disponibles");
+console.log("Leche,azucar,yogurt, manteca, queso, fideos, salsa de tomate,aceite, crema de leche, dulce de leche, galletitas");
+console.log("stock de los productos disponibles");
+console.log("4 packs de leche, 1 pack de azucar, 5 yogures, 4 mantecas, 4 quesos, 10 fideos, 10 salsa de tomates, 4 crema de leches, 6 paquetes de galletitas") ;
