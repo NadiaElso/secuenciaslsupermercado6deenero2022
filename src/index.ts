ec31982e8a1d9a4e8fe758d2d560bd78d14b8f7e
NadiaElso <92063948+NadiaElso@users.noreply.github.com>
2022-01-10 20:21:46 -0300

    clase10deenero20221

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,41 @@
-console.log("Supermercado Mirta");
+console.log("Supermercado Madariaga");
 console.log("productos disponibles");
-console.log("Leche,azucar,yogurt, manteca, queso, fideos, salsa de tomate,aceite, crema de leche, dulce de leche, galletitas");
+console.log(
+  "Leche,azucar,yogurt, manteca, queso, fideos, salsa de tomate,aceite, crema de leche, dulce de leche, galletitas"
+);
 console.log("stock de los productos disponibles");
-console.log("4 packs de leche, 1 pack de azucar, 5 yogures, 4 mantecas, 4 quesos, 10 fideos, 10 salsa de tomates, 4 crema de leches, 6 paquetes de galletitas") ;
+console.log(
+  "4 packs de leche, 1 pack de azucar, 5 yogures, 4 mantecas, 4 quesos, 10 fideos, 10 salsa de tomates, 4 crema de leches, 6 paquetes de galletitas"
+);
 
+let nombreDesupermercado: string;
+let productosDisponibles: string;
+let stockLeche: number;
+let stockAzucar: number;
+let stockYogurt: number;
+let stockManteca: number;
+let stockQueso: number;
+let stockFideos: number;
+let stockSalsaDeTomate: number;
+let StockAceite: number;
+let StockCremaDeLeche: number;
+let stockDulceDeLeche: number;
+let stockGalletitas: number;
 
-let nombreDesupermercado:string;
-let productosDisponibles:string;
-let stockLeche:number;
-let stockAzucar:number;
-let stockYogurt:number;
-let stockManteca:number;
-let stockQueso:number;
-let stockFideos:number;
-let stockSalsaDeTomate:number;
-let StockAceite:number;
-let StockCremaDeLeche:number;
-let stockDulceDeLeche:number;
-let stockGalletitas:number;
-
-
-
-nombreDesupermercado="Mirta";
-productosDisponibles="Leche,Azucar, Yogurt, manteca, queso, fideos, salsa de tomate, aceite, crema de leche, dulce de leche, galletitas";
-stockLeche= 4;
-stockAzucar= 1;
-stockYogurt= 5;
-stockManteca= 4;
-stockQueso= 4;
-stockFideos=10;
-stockSalsaDeTomate= 10;
-StockCremaDeLeche=4;
-stockDulceDeLeche=0;
-stockGalletitas=6;
+nombreDesupermercado = "Madariaga";
+productosDisponibles =
+  "Leche,Azucar, Yogurt, manteca, queso, fideos, salsa de tomate, aceite, crema de leche, dulce de leche, galletitas";
+stockLeche = 4;
+stockAzucar = 1;
+stockYogurt = 5;
+stockManteca = 4;
+stockQueso = 4;
+stockFideos = 10;
+stockSalsaDeTomate = 10;
+StockAceite = 0;
+StockCremaDeLeche = 4;
+stockDulceDeLeche = 0;
+stockGalletitas = 6;
 
 console.log(nombreDesupermercado);
 console.log(productosDisponibles);
