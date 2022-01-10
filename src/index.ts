176ab7bf1d9795678badeaa6523abdae2ec6079c
NadiaElso <92063948+NadiaElso@users.noreply.github.com>
2022-01-10 20:15:56 -0300

    clase10deenero2022

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,47 @@
-console.log("Supermercado");
+console.log("Supermercado Mirta");
 console.log("productos disponibles");
 console.log("Leche,azucar,yogurt, manteca, queso, fideos, salsa de tomate,aceite, crema de leche, dulce de leche, galletitas");
 console.log("stock de los productos disponibles");
 console.log("4 packs de leche, 1 pack de azucar, 5 yogures, 4 mantecas, 4 quesos, 10 fideos, 10 salsa de tomates, 4 crema de leches, 6 paquetes de galletitas") ;
+
+
+let nombreDesupermercado:string;
+let productosDisponibles:string;
+let stockLeche:number;
+let stockAzucar:number;
+let stockYogurt:number;
+let stockManteca:number;
+let stockQueso:number;
+let stockFideos:number;
+let stockSalsaDeTomate:number;
+let StockAceite:number;
+let StockCremaDeLeche:number;
+let stockDulceDeLeche:number;
+let stockGalletitas:number;
+
+
+
+nombreDesupermercado="Mirta";
+productosDisponibles="Leche,Azucar, Yogurt, manteca, queso, fideos, salsa de tomate, aceite, crema de leche, dulce de leche, galletitas";
+stockLeche= 4;
+stockAzucar= 1;
+stockYogurt= 5;
+stockManteca= 4;
+stockQueso= 4;
+stockFideos=10;
+stockSalsaDeTomate= 10;
+StockCremaDeLeche=4;
+stockDulceDeLeche=0;
+stockGalletitas=6;
+
+console.log(nombreDesupermercado);
+console.log(productosDisponibles);
+console.log(stockLeche);
+console.log(stockAzucar);
+console.log(stockManteca);
+console.log(stockQueso);
+console.log(stockFideos);
+console.log(stockSalsaDeTomate);
+console.log(StockCremaDeLeche);
+console.log(stockDulceDeLeche);
+console.log(stockGalletitas);
